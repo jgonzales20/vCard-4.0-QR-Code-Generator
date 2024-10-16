ae9b7be6b391b472435a3818ff8c66dc0722f1e2
Jacob Gonzales <jgonzales@garrisondental.com>
2024-10-16 17:20:01 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vCard4.0-QRCode-Generator/',
+  base: '/vcard-generator-qr/',
   build: {
     outDir: 'docs'
   }
